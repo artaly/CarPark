4ce60d912b192ab1a9bb1c46fd5ad5c791a47d5c
LAETICIA HERMOSA <villalonalycah@gmail.com>
2022-01-23 00:20:52 +0800

    Added invoice stuff

--- CarPark/Interface/Receipt.Designer.cs
+++ CarPark/Interface/Receipt.Designer.cs
@@ -33,262 +33,18 @@ namespace CarPark
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Receipt));
-            this.gpbInput = new System.Windows.Forms.GroupBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.btnDelete = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblTimeout = new System.Windows.Forms.Label();
-            this.lblTimeIn = new System.Windows.Forms.Label();
-            this.lblColor = new System.Windows.Forms.Label();
-            this.lblBrand = new System.Windows.Forms.Label();
-            this.lblLicense = new System.Windows.Forms.Label();
             this.lblClose = new System.Windows.Forms.PictureBox();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.gpbInput.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.lblClose)).BeginInit();
             this.SuspendLayout();
-            // 
-            // gpbInput
-            // 
-            this.gpbInput.Controls.Add(this.label8);
-            this.gpbInput.Controls.Add(this.label9);
-            this.gpbInput.Controls.Add(this.label2);
-            this.gpbInput.Controls.Add(this.label3);
-            this.gpbInput.Controls.Add(this.label4);
-            this.gpbInput.Controls.Add(this.label5);
-            this.gpbInput.Controls.Add(this.label6);
-            this.gpbInput.Controls.Add(this.label7);
-            this.gpbInput.Controls.Add(this.btnDelete);
-            this.gpbInput.Controls.Add(this.label1);
-            this.gpbInput.Controls.Add(this.lblTimeout);
-            this.gpbInput.Controls.Add(this.lblTimeIn);
-            this.gpbInput.Controls.Add(this.lblColor);
-            this.gpbInput.Controls.Add(this.lblBrand);
-            this.gpbInput.Controls.Add(this.lblLicense);
-            this.gpbInput.Location = new System.Drawing.Point(40, 46);
-            this.gpbInput.Name = "gpbInput";
-            this.gpbInput.Size = new System.Drawing.Size(382, 501);
-            this.gpbInput.TabIndex = 0;
-            this.gpbInput.TabStop = false;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label8.Location = new System.Drawing.Point(197, 361);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(94, 33);
-            this.label8.TabIndex = 27;
-            this.label8.Text = "VALUE";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.BackColor = System.Drawing.Color.Transparent;
-            this.label9.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label9.Location = new System.Drawing.Point(18, 361);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(165, 33);
-            this.label9.TabIndex = 26;
-            this.label9.Text = "RECEIPT NO:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(197, 209);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 33);
-            this.label2.TabIndex = 25;
-            this.label2.Text = "VALUE";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(197, 312);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 33);
-            this.label3.TabIndex = 24;
-            this.label3.Text = "VALUE";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(197, 263);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 33);
-            this.label4.TabIndex = 23;
-            this.label4.Text = "VALUE";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label5.Location = new System.Drawing.Point(197, 153);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 33);
-            this.label5.TabIndex = 22;
-            this.label5.Text = "VALUE";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label6.Location = new System.Drawing.Point(197, 100);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(94, 33);
-            this.label6.TabIndex = 21;
-            this.label6.Text = "VALUE";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label7.Location = new System.Drawing.Point(197, 42);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 33);
-            this.label7.TabIndex = 20;
-            this.label7.Text = "VALUE";
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(93)))), ((int)(((byte)(81)))));
-            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(178)))), ((int)(((byte)(167)))));
-            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnDelete.BorderRadius = 4;
-            this.btnDelete.ButtonText = "OK";
-            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDelete.DisabledColor = System.Drawing.Color.Gray;
-            this.btnDelete.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnDelete.Iconimage = null;
-            this.btnDelete.Iconimage_right = null;
-            this.btnDelete.Iconimage_right_Selected = null;
-            this.btnDelete.Iconimage_Selected = null;
-            this.btnDelete.IconMarginLeft = 0;
-            this.btnDelete.IconMarginRight = 0;
-            this.btnDelete.IconRightVisible = true;
-            this.btnDelete.IconRightZoom = 0D;
-            this.btnDelete.IconVisible = true;
-            this.btnDelete.IconZoom = 90D;
-            this.btnDelete.IsTab = false;
-            this.btnDelete.Location = new System.Drawing.Point(126, 427);
-            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(20)))), ((int)(((byte)(38)))));
-            this.btnDelete.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(30)))), ((int)(((byte)(49)))));
-            this.btnDelete.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnDelete.selected = false;
-            this.btnDelete.Size = new System.Drawing.Size(129, 44);
-            this.btnDelete.TabIndex = 8;
-            this.btnDelete.Text = "OK";
-            this.btnDelete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnDelete.Textcolor = System.Drawing.Color.White;
-            this.btnDelete.TextFont = new System.Drawing.Font("Open Sans Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(50, 209);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 33);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "AMOUNT:";
-            // 
-            // lblTimeout
-            // 
-            this.lblTimeout.AutoSize = true;
-            this.lblTimeout.BackColor = System.Drawing.Color.Transparent;
-            this.lblTimeout.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTimeout.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblTimeout.Location = new System.Drawing.Point(45, 312);
-            this.lblTimeout.Name = "lblTimeout";
-            this.lblTimeout.Size = new System.Drawing.Size(136, 33);
-            this.lblTimeout.TabIndex = 7;
-            this.lblTimeout.Text = "TIME OUT:";
-            // 
-            // lblTimeIn
-            // 
-            this.lblTimeIn.AutoSize = true;
-            this.lblTimeIn.BackColor = System.Drawing.Color.Transparent;
-            this.lblTimeIn.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTimeIn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblTimeIn.Location = new System.Drawing.Point(71, 263);
-            this.lblTimeIn.Name = "lblTimeIn";
-            this.lblTimeIn.Size = new System.Drawing.Size(112, 33);
-            this.lblTimeIn.TabIndex = 6;
-            this.lblTimeIn.Text = "TIME IN:";
-            // 
-            // lblColor
-            // 
-            this.lblColor.AutoSize = true;
-            this.lblColor.BackColor = System.Drawing.Color.Transparent;
-            this.lblColor.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColor.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblColor.Location = new System.Drawing.Point(32, 153);
-            this.lblColor.Name = "lblColor";
-            this.lblColor.Size = new System.Drawing.Size(151, 33);
-            this.lblColor.TabIndex = 5;
-            this.lblColor.Text = "DURATION:";
-            // 
-            // lblBrand
-            // 
-            this.lblBrand.AutoSize = true;
-            this.lblBrand.BackColor = System.Drawing.Color.Transparent;
-            this.lblBrand.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBrand.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblBrand.Location = new System.Drawing.Point(97, 100);
-            this.lblBrand.Name = "lblBrand";
-            this.lblBrand.Size = new System.Drawing.Size(86, 33);
-            this.lblBrand.TabIndex = 3;
-            this.lblBrand.Text = "DATE:";
-            // 
-            // lblLicense
-            // 
-            this.lblLicense.AutoSize = true;
-            this.lblLicense.BackColor = System.Drawing.Color.Transparent;
-            this.lblLicense.Font = new System.Drawing.Font("Corbel", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLicense.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.lblLicense.Location = new System.Drawing.Point(50, 42);
-            this.lblLicense.Name = "lblLicense";
-            this.lblLicense.Size = new System.Drawing.Size(141, 33);
-            this.lblLicense.TabIndex = 1;
-            this.lblLicense.Text = "PLATE NO:";
             // 
             // lblClose
             // 
             this.lblClose.Image = ((System.Drawing.Image)(resources.GetObject("lblClose.Image")));
-            this.lblClose.Location = new System.Drawing.Point(430, 12);
+            this.lblClose.Location = new System.Drawing.Point(573, 15);
+            this.lblClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lblClose.Name = "lblClose";
-            this.lblClose.Size = new System.Drawing.Size(28, 28);
+            this.lblClose.Size = new System.Drawing.Size(37, 34);
             this.lblClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.lblClose.TabIndex = 10;
             this.lblClose.TabStop = false;
@@ -300,46 +56,27 @@ namespace CarPark
             // 
             // Receipt
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
-            this.ClientSize = new System.Drawing.Size(470, 593);
+            this.ClientSize = new System.Drawing.Size(627, 730);
             this.Controls.Add(this.lblClose);
-            this.Controls.Add(this.gpbInput);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Receipt";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Dashboard_Load);
-            this.gpbInput.ResumeLayout(false);
-            this.gpbInput.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.lblClose)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox gpbInput;
-        private System.Windows.Forms.Label lblColor;
-        private System.Windows.Forms.Label lblBrand;
-        private System.Windows.Forms.Label lblLicense;
-        private System.Windows.Forms.Label lblTimeout;
-        private System.Windows.Forms.Label lblTimeIn;
-        private Bunifu.Framework.UI.BunifuFlatButton btnDelete;
         private System.Windows.Forms.PictureBox lblClose;
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
     }
 }
 
