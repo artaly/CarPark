a62c503840509b2e080b723018534c827391e6ed
LAETICIA HERMOSA <villalonalycah@gmail.com>
2022-01-26 00:03:28 +0800

    Deleted export excel

--- CarPark/Interface/PolicyList.Designer.cs
+++ CarPark/Interface/PolicyList.Designer.cs
@@ -30,47 +30,28 @@ namespace CarPark.Interface
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PolicyList));
-            this.btnExcel = new Bunifu.Framework.UI.BunifuImageButton();
             this.btnSearch = new System.Windows.Forms.Button();
             this.dtpToDate = new System.Windows.Forms.DateTimePicker();
             this.dtpFromDate = new System.Windows.Forms.DateTimePicker();
             this.label7 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.dgtTransactionDetails = new System.Windows.Forms.DataGridView();
             this.btnReturn = new System.Windows.Forms.Button();
             this.lblForTotal = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
             this.lblClose = new System.Windows.Forms.PictureBox();
             this.btnTransfer = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.lblNo = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.btnExcel)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgtTransactionDetails)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lblClose)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnExcel
-            // 
-            this.btnExcel.BackColor = System.Drawing.Color.Transparent;
-            this.btnExcel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnExcel.Image = ((System.Drawing.Image)(resources.GetObject("btnExcel.Image")));
-            this.btnExcel.ImageActive = null;
-            this.btnExcel.Location = new System.Drawing.Point(507, 71);
-            this.btnExcel.Margin = new System.Windows.Forms.Padding(4);
-            this.btnExcel.Name = "btnExcel";
-            this.btnExcel.Size = new System.Drawing.Size(71, 59);
-            this.btnExcel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.btnExcel.TabIndex = 31;
-            this.btnExcel.TabStop = false;
-            this.btnExcel.Zoom = 10;
-            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
             // 
             // btnSearch
             // 
@@ -122,18 +103,6 @@ namespace CarPark.Interface
             this.label7.Size = new System.Drawing.Size(55, 18);
             this.label7.TabIndex = 23;
             this.label7.Text = "To Date";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.DimGray;
-            this.label3.Location = new System.Drawing.Point(491, 134);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 18);
-            this.label3.TabIndex = 24;
-            this.label3.Text = "Export to Excel";
             // 
             // label2
             // 
@@ -197,6 +166,46 @@ namespace CarPark.Interface
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1071, 385);
             this.panel1.TabIndex = 33;
+            // 
+            // lblNo
+            // 
+            this.lblNo.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblNo.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNo.Location = new System.Drawing.Point(987, 0);
+            this.lblNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.lblNo.Size = new System.Drawing.Size(84, 57);
+            this.lblNo.TabIndex = 37;
+            this.lblNo.Text = "0";
+            this.lblNo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label8.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(855, 0);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.label8.Size = new System.Drawing.Size(140, 57);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Abandoned ID #:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label5.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
+            this.label5.Size = new System.Drawing.Size(883, 57);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Abandoned List (Cars present here are denied entry, unless customer pays the bala" +
+    "nce)";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // dgtTransactionDetails
             // 
@@ -290,46 +299,6 @@ namespace CarPark.Interface
             this.btnTransfer.UseVisualStyleBackColor = false;
             this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
             // 
-            // label5
-            // 
-            this.label5.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.label5.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(0, 0);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
-            this.label5.Size = new System.Drawing.Size(883, 57);
-            this.label5.TabIndex = 35;
-            this.label5.Text = "Abandoned List (Cars present here are denied entry, unless customer pays the bala" +
-    "nce)";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label8
-            // 
-            this.label8.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.label8.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(855, 0);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
-            this.label8.Size = new System.Drawing.Size(140, 57);
-            this.label8.TabIndex = 36;
-            this.label8.Text = "Abandoned ID #:";
-            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // lblNo
-            // 
-            this.lblNo.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.lblNo.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNo.Location = new System.Drawing.Point(987, 0);
-            this.lblNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblNo.Name = "lblNo";
-            this.lblNo.Padding = new System.Windows.Forms.Padding(13, 0, 0, 0);
-            this.lblNo.Size = new System.Drawing.Size(84, 57);
-            this.lblNo.TabIndex = 37;
-            this.lblNo.Text = "0";
-            this.lblNo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // PolicyList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -342,12 +311,10 @@ namespace CarPark.Interface
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.lblClose);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.btnExcel);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.dtpToDate);
             this.Controls.Add(this.dtpFromDate);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
@@ -357,7 +324,6 @@ namespace CarPark.Interface
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TransactionSales";
             this.Load += new System.EventHandler(this.PolicyList_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.btnExcel)).EndInit();
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgtTransactionDetails)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.lblClose)).EndInit();
@@ -367,13 +333,10 @@ namespace CarPark.Interface
         }
 
         #endregion
-
-        private Bunifu.Framework.UI.BunifuImageButton btnExcel;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.DateTimePicker dtpToDate;
         private System.Windows.Forms.DateTimePicker dtpFromDate;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
